7199ed972090533265b5ae2bf6fba3c84c54e4d7
BOTOOM <edwardiaz.dev@gmail.com>
2024-02-20 20:07:50 -0500

    feat: create layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,54 @@
-import Image from "next/image";
+// import { Alert } from "flowbite-react";
+import Table from "@/components/table";
+import { Flowbite } from "flowbite-react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      holi
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Flowbite>
+        <div className="grid grid-cols-12 gap-4">
+          <div className="col-span-12 md: col-span-9">
+            {/* Lista de Pokémon */}
+            {/* lista */}
+            <div className="bg-gray-100 rounded-md p-4">
+              <Table></Table>
+              {/* Aquí irá la lista de Pokémon */}
+            </div>
+          </div>
+          {/* {selectedPokemon && (
+          <div className="col-span-12 md:col-span-3">
+            Detalles del Pokémon
+            <div className="bg-gray-100 rounded-md p-4">
+              Aquí irán los detalles del Pokémon
+            </div>
+          </div> 
+        )} */}
+
+          <div className="col-span-12 md:col-span-3">
+            {/* Detalles del Pokémon */}
+            <div className="bg-gray-100 rounded-md p-4">
+              {/* Aquí irán los detalles del Pokémon */}
+              detalle
+            </div>
+          </div>
+        </div>
+
+        {/* Modal para dispositivos móviles */}
+        {/* {modalOpen && selectedPokemon && (
+          <Flowbite.Modal
+            size="xl"
+            open={modalOpen}
+            onClose={() => {
+              setModalOpen(false);
+              setSelectedPokemon(null);
+            }}
+          >
+            <Flowbite.ModalBody>
+              Aquí irán los detalles del Pokémon
+            </Flowbite.ModalBody>
+          </Flowbite.Modal>
+        )} */}
+      </Flowbite>
     </main>
   );
 }
---
+++ src/components/nav.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+import logo from "@/assets/siteLogo.svg"
+
+
+export default function NavBar() {
+  return (
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a
+          href="https://flowbite.com/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          {/* <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="h-8"
+            alt="Flowbite Logo"
+          /> */}
+          <Image src={logo} alt="site logo" width={30} height={30} />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            PokeDex
+          </span>
+        </a>
+        
+      </div>
+    </nav>
+  );
+}
